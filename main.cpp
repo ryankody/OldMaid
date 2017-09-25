101b9c66767d21486de47cbf6cb0ff94a13e0982
Ryan <rsk35@zips.uakron.edu>
2017-09-25 13:59:53 -0400

    init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "card.hpp"
 #include "deck.hpp"
-#include "game.hpp"
 #include "player.hpp"
 
 #include <iostream>
@@ -18,28 +17,6 @@ std::minstd_rand prng;
 int main()
 {
 	prng.seed(rng());
-
-	Game g;
-
-	g.deck = makeStandardDeck();
-	shuffleDeck(g.deck);
-	dealDeck(g.deck, g.p1, g.p2);
-
-
-	Card p1Card;
-	Card p2Card;
-
-	while(!g.p1.empty() || !g.p2.empty())
-	{
-		p1Card = g.p1.front();
-		g.p1.pop();
-
-		p2Card = g.p2.front();
-		g.p2.pop();
-
-	}
-
-
 
 	return 0;
 }
--- player.cpp
+++ player.cpp
@@ -2,14 +2,4 @@
 
 void printPlayer(Player p)
 {
-	int i = 0;
-
-	while(!p.empty())
-	{
-		i++;
-		std::cout << p.front() << '\n';
-		p.pop();
-	}
-
-	std::cout << i;
 }
